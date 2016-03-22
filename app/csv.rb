9193466a56f3e23fd7bf8ac6376aa52d143f0080
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-03-22 18:26:21 -0300

    The csv must be immutable

--- app/csv.rb
+++ app/csv.rb
@@ -2,8 +2,8 @@ class Csv
   attr_accessor :file_path
 
   def initialize(file_path)
-    file_path += '.csv' unless /\A.*?\.csv\z/ =~ file_name
+    file_path += '.csv' unless /\A.*?\.csv\z/ =~ file_path
 
-    @file_path = file_path
+    @file_path = file_path.freeze
   end
 end
--- spec/model/csv_spec.rb
+++ spec/model/csv_spec.rb
@@ -16,5 +16,22 @@ describe Csv, :model do
       csv = Csv.new('../folder/a.file.csv')
       expect(csv.file_path).to eq('../folder/a.file.csv')
     end
+
+    it 'cannot modify the original string' do
+      file_path = 'the_file'
+      csv = Csv.new file_path
+
+      expect(file_path).to eq('the_file')
+      expect(csv.file_path).to eq('the_file.csv')
+    end
+  end
+
+  describe '#file_path' do
+    it 'must be immutable' do
+      csv = Csv.new('../folder/a.file.csv')
+      expect {
+        csv.file_path << 'teste'
+      }.to raise_error(RuntimeError, "can't modify frozen String")
+    end
   end
 end
