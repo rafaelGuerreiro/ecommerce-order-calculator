701e07d1464c4bcd7793e59ccd002d2b454a45e7
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-03-22 20:32:42 -0300

    Add spec helper and required entire app folder.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rspec'
 gem 'rspec-collection_matchers'
 gem 'rubocop'
+gem 'require_all'
 
 gem 'byebug', group: :development
 gem 'codeclimate-test-reporter', group: :test, require: nil
--- spec/model/csv_spec.rb
+++ spec/model/csv_spec.rb
@@ -1,8 +1,3 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
-require_relative '../../app/model/csv.rb'
-
 describe Csv, :model do
   describe '.new' do
     it 'allows file path without extension' do
