a64bccd4e8d768673073fcb2b3a5c34094ec4972
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-03-25 23:25:13 -0300

    Use the semantic method

--- app/model/order.rb
+++ app/model/order.rb
@@ -3,6 +3,6 @@ class Order < CsvModel::Base
   define_field :coupon, references: Coupon, presence: false
 
   def products
-    @products ||= OrderProduct.find_by(order_id: @id).map(&:product)
+    @products ||= OrderProduct.find_by_order_id(@id).map(&:product)
   end
 end
