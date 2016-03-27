bfe0452a37e6aba2893d24c41acc80938477f827
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-03-27 11:24:58 -0300

    Remove unused variable

--- spec/helper/stub_csv_file.rb
+++ spec/helper/stub_csv_file.rb
@@ -3,7 +3,7 @@ module CsvFileHelper
     allow_call_original
 
     allow(File).to receive(:exist?).with(file_path).and_return(true)
-    stub = allow(CSV).to receive(:read).with(file_path)
+    allow(CSV).to receive(:read).with(file_path)
       .and_return(csv_data.map { |row| row.split(',') })
   end
 
