f26ebde2261ddc81bd184516c7b1ae3d33914e66
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-03-27 16:00:18 -0300

    Add test for Application#serialize_result

--- spec/integration/application_integration_spec.rb
+++ spec/integration/application_integration_spec.rb
@@ -1,15 +1,33 @@
 describe Application, :integration do
   describe '#serialize_result' do
-    it 'customizes the order for the result' do
+    it 'customizes the orders for the result' do
       paths = %w(coupons products orders order_items).map do |file|
         "spec/integration/csvs/#{file}"
       end
-      paths << 'spec/integration/csvs/result.csv'
+      paths << result_path = '/csvs/result.csv'
 
       app = Application.new paths
       app.load_models
 
-      # app.serialize_result
+      target = stub_csv_writing_target result_path
+
+      app.serialize_result do |file_path|
+        expect(file_path).to eq(result_path)
+      end
+
+      expect(target).to contain_exactly(
+        [123, 2_250.0],
+        [234, 216.75],
+        [345, 314.5],
+        [456, 425.0],
+        [567, 525.0],
+        [678, 2_227.5],
+        [789, 12_460.0],
+        [890, 300.0],
+        [987, 150.0],
+        [1000, 0.0],
+        [1001, 0.0]
+      )
     end
   end
 end
