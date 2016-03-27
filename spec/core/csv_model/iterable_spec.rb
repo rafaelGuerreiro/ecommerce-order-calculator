f88626ebaebb6152c8bece09283ba469c5c95bfe
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-03-27 10:50:40 -0300

    Add tests for CsvModel::Iterable each, each_with_index and count

--- spec/core/csv_model/iterable_spec.rb
+++ spec/core/csv_model/iterable_spec.rb
@@ -38,11 +38,70 @@ describe CsvModel::Iterable do
         expect(ids.include?(person.id)).to be_truthy
       end
     end
+
+    it "does nothing when there's no record" do
+      CsvModelRepository.destroy!
+
+      expect do
+        Person.each do |person|
+          raise "Error, this shouldn't be reached. Model => #{person}."
+        end
+      end.to_not raise_error
+    end
   end
 
   describe '.each_with_index' do
+    it 'responds to each_with_index method' do
+      expect(Person.respond_to?(:each_with_index)).to be_truthy
+    end
+
+    it 'iterates over all valid models in the same order they were saved' do
+      ids = [123, 234, 345, 456, 678]
+
+      Person.each_with_index do |person, index|
+        expect(person).to be_valid
+        expect(person.id).to eq(ids[index])
+      end
+    end
+
+    it "does nothing when there's no record" do
+      CsvModelRepository.destroy!
+
+      expect do
+        Person.each_with_index do |person, index|
+          raise "Error, this shouldn't be reached. " \
+            "Model => #{person} at index #{index}."
+        end
+      end.to_not raise_error
+    end
   end
 
   describe '.count' do
+    it 'responds to count method' do
+      expect(Person.respond_to?(:count)).to be_truthy
+    end
+
+    it 'counts only valid models' do
+      expect(Person.count).to eq(5)
+    end
+
+    it 'accepts a block to filter counting' do
+      expect(Person.count do |person|
+        person.name.include?('o')
+      end).to eq(4)
+    end
+
+    it 'accepts a model to count' do
+      clotilde = Person.find(234)
+      expect(Person.count(clotilde)).to eq(1)
+
+      expect(Person.count(nil)).to eq(0)
+    end
+
+    it "returns zero when there's no record" do
+      CsvModelRepository.destroy!
+
+      expect(Person.count).to eq(0)
+    end
   end
 end
